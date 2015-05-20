48a3a3728715112b80b82c705e51994b71680470
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-05-20 15:46:19 -0400

    Maven Using slf4j

--- src/main/java/org/jmatharu/maven/App.java
+++ src/main/java/org/jmatharu/maven/App.java
@@ -1,5 +1,6 @@
 package org.jmatharu.maven;
 
+import org.slf4j.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        //using Logger Oblect of slf4j
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello world !!");
     }
 }
